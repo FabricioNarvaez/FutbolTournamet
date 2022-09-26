8ca28af6f27fea4579e37c145ea6170cad716f0b
Fabricio <tekendefabri@gmail.com>
2022-09-26 19:55:14 +0200

    Home and error components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { routing, appRoutingProviders } from './app.routing';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ErrorComponent } from './components/error/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,8 +1,12 @@
 import { ModuleWithProviders } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
-const appRoutes: Routes = [
+import { HomeComponent } from "./components/home/home.component";
+import { ErrorComponent } from "./components/error/error.component";
 
+const appRoutes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: '**', component: ErrorComponent}
 ];
 
 export const appRoutingProviders: any[] = [];
