805bb8f1f2ad7b0a904f20a92804719ce982c9d3
FabricioNarvaez <narvaezfabricio38@gmail.com>
2023-02-17 20:10:34 +0100

    Added swipper on home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import SwiperCore, { Navigation, Pagination} from 'swiper';
+
+SwiperCore.use([Navigation, Pagination]);
+
 
 @Component({
-  selector: 'home',
+  selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class HomeComponent implements OnInit {
 
