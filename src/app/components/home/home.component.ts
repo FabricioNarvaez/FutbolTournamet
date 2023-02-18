90d2301b22ca19b84c5d943b480dcc5f8844dd81
FabricioNarvaez <narvaezfabricio38@gmail.com>
2023-02-18 21:13:26 +0100

    Added table and header for swiper

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,20 +1,28 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import SwiperCore, { Navigation, Pagination} from 'swiper';
+import SwiperCore, { Navigation, Pagination, SwiperOptions } from 'swiper';
 
 SwiperCore.use([Navigation, Pagination]);
-
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
+  swiperConfig: SwiperOptions = {
+    slidesPerView: "auto",
+    spaceBetween: 10,
+    loop: true,
+    navigation: true,
+    pagination: { clickable: false },
+    breakpoints: {
+      6000: {slidesPerView: 2},
+      1000: {slidesPerView: 4},
+    },
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
