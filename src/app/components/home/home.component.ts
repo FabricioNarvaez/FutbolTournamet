61190c3d43b54dcd306640b73ddd671fb04c3934
FabricioNarvaez <narvaezfabricio38@gmail.com>
2023-02-20 20:51:17 +0100

    Load teams img and name from ts file and functional swiper breakpoints

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,19 +7,70 @@ SwiperCore.use([Navigation, Pagination]);
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
-  encapsulation: ViewEncapsulation.None,
+  encapsulation: ViewEncapsulation.None
 })
-export class HomeComponent {
-  swiperConfig: SwiperOptions = {
+export class HomeComponent implements OnInit{
+
+  imgCollection: Array<any> = [
+    {
+      src: "../../../assets/images/teams/Arsenal.png",
+      club: "Arsenal"
+    },
+    {
+      src: "../../../assets/images/teams/Barcelona.png",
+      club: "Barcelona"
+    },
+    {
+      src: "../../../assets/images/teams/Bayern.png",
+      club: "Bayern"
+    },
+    {
+      src: "../../../assets/images/teams/Besiktas.png",
+      club: "Besiktas"
+    },
+    {
+      src: "../../../assets/images/teams/Borussia.png",
+      club: "Borussia"
+    },
+    {
+      src: "../../../assets/images/teams/Club Nacional.png",
+      club: "Club Nacional"
+    },
+    {
+      src: "../../../assets/images/teams/Diogo.png",
+      club: "Diogo"
+    },
+    {
+      src: "../../../assets/images/teams/Neira.png",
+      club: "Neira"
+    },
+    {
+      src: "../../../assets/images/teams/PSG.png",
+      club: "PSG"
+    },
+  ]
+
+  swiperConfig: any = {
+    // centeredSlides: "true",
     slidesPerView: "auto",
-    spaceBetween: 10,
+    spaceBetween: 0,
     loop: true,
     navigation: true,
     pagination: { clickable: false },
     breakpoints: {
-      6000: {slidesPerView: 2},
-      1000: {slidesPerView: 4},
-    },
+      '@0.75': {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      '@1.00': {
+        slidesPerView: 4,
+        spaceBetween: 40,
+      },
+      '@1.50': {
+        slidesPerView: 5,
+        spaceBetween: 50,
+      },
+    }
   };
 
   constructor() {}
