c2b1223b7d9512d81b6b101f42cfe295ee5cb9df
FabricioNarvaez <narvaezfabricio38@gmail.com>
2023-03-09 19:46:07 +0100

    [ClubPage]: Created class for inline nav items and added nav to club page

--- src/app/components/clubPage/clubPage.component.ts
+++ src/app/components/clubPage/clubPage.component.ts
@@ -3,17 +3,33 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-club-page',
   templateUrl: './clubPage.component.html',
-  styleUrls: ['./clubPage.component.css']
+  styleUrls: ['./clubPage.component.css'],
 })
 export class ClubPageComponent implements OnInit {
   clubInfo = {
-      img: "../../../assets/images/teams/Bayern.png",
-      clubName: "Bayern",
-      backgroundColor: "red"
-    }
-  constructor() { }
+    img: '../../../assets/images/teams/Bayern.png',
+    clubName: 'Bayern',
+    backgroundColor: 'red',
+  };
 
-  ngOnInit(): void {
-  }
-
+  links: Array<any> = [
+    {
+      route: '/club/general',
+      title: 'General',
+    },
+    {
+      route: '/club/plantilla',
+      title: 'Plantilla',
+    },
+    {
+      route: '/club/Estadisticas',
+      title: 'Estadísticas',
+    },
+    {
+      route: '/club/resultados',
+      title: 'Resultados',
+    },
+  ];
+  constructor() {}
+  ngOnInit(): void {}
 }
