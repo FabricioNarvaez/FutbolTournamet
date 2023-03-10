c147fb37300e541e15618f57255fedccdc50ee97
FabricioNarvaez <narvaezfabricio38@gmail.com>
2023-03-10 17:22:51 +0100

    [ClubPage]: Created next match component

---
+++ src/app/components/clubPageGeneralComponents/nextMatch/nextMatch.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nextMatch',
+  templateUrl: './nextMatch.component.html',
+  styleUrls: ['./nextMatch.component.css']
+})
+export class NextMatchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
